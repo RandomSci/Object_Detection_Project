4b5a03dd06bf06712a2f782c5fc51e34a1b79ded
Selwyn <jaymeselwyn@gmail.com>
2024-11-28 10:48:43 +0800

    Updates

--- app.py
+++ app.py
@@ -3,11 +3,13 @@ from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 import os
+from ultralytics import YOLO
+import cv2
 
 app = FastAPI()
+model = YOLO("yolo11n.pt")
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
-
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
@@ -17,18 +19,33 @@ async def home(request: Request):
 @app.post("/upload")
 async def upload(request: Request, file: UploadFile = File(...)):
     os.makedirs("static/uploaded", exist_ok=True)
+    os.makedirs("static/saved", exist_ok=True)
+
 
     img_path = os.path.join("static/uploaded", file.filename)
-
     with open(img_path, "wb") as f:
         f.write(await file.read())
 
-    result = "Pogi"
-    
+    img = cv2.imread(img_path) 
+    results = model(img)        
+    boxes = results[0].boxes.xyxy.cpu().numpy()  
+    scores = results[0].boxes.conf.cpu().numpy() 
+
+    for box, score in zip(boxes, scores):
+        x1, y1, x2, y2 = map(int, box)
+        cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+        label = f"{score:.2f}"
+        cv2.putText(img, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
+    processed_img_path = os.path.join("static/saved", file.filename)
+    cv2.imwrite(processed_img_path, img)
+
     return templates.TemplateResponse(
-        "home.html", 
-        {"request": request, 
-        "Message": "Image Uploaded!", 
-        "img_path": f"/static/uploaded/{file.filename}", 
-        "result": f"Projected Object: {result}"}
+        "home.html",
+        {
+            "request": request,
+            "Message": "Image Uploaded and Processed!",
+            "uploaded_img_path": f"/static/uploaded/{file.filename}",
+            "processed_img_path": f"/static/saved/{file.filename}",
+        }
     )
